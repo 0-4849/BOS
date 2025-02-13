662bf2ab0aae225e363797ad94caa526d32e0b8d
Tars <tars.nijman@gmail.com>
2025-02-13 08:53:13 +0100

    initial commit

---
+++ bird_facts.py
@@ -0,0 +1,17 @@
+import pyreadr
+import pandas as pd
+
+print("parsing data")
+res = pyreadr.read_r("dietdb.rda")["dietdb"]
+print(f"Table columns: {res.keys()}")
+species = res["Common_Name"].drop_duplicates()
+# print(species)
+# print(res.loc[res["Common_Name"] == species[0]])
+
+def find_commonest_food(data: pd.DataFrame, species: str) -> (str, int):
+    entries = data.loc[data["Common_Name"] == species]
+    total_items = entries["Item_Sample_Size"]
+    food_frequency = entries["Prey_Class"].value_counts()
+    return (food_frequency.index[0], food_frequency.iloc[0])
+
+print(find_commonest_food(res, "Bald Eagle"))
