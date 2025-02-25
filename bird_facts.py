7ead9f8db76e9374be8e295786b905daad892364
Tars <tars.nijman@gmail.com>
2025-02-25 13:16:29 +0100

    added comments + started working on Card class

--- bird_facts.py
+++ bird_facts.py
@@ -1,22 +1,49 @@
-import pyreadr
+import random
+import numpy as np
 import pandas as pd
+import pyreadr
 
-print("parsing data")
-data = pyreadr.read_r("dietdb.rda")["dietdb"]
-print(f"Table columns: {data.keys()}")
-species = data["Common_Name"].drop_duplicates()
-# print(species)
-# print(res.loc[res["Common_Name"] == species[0]])
+class Card:
+    def __init__(self, question: str, answers: list[str], correct_answer_index: int):
+        self.question = question
+        self.answers = answers
+        self.correct_answer_index = correct_answer_index 
 
-def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, int):
+    def __str__(self):
+        return self.question + "\n" + "\n".join([f"{l}: {a}" for l, a in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", random.sample(self.answers, len(self.answers)))])
+        
+
+def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, np.float64):
     global data
     entries = data.loc[(data["Common_Name"] == species) & (data["Diet_Type"] == "Items")]
+	
+	# sometimes, the eaten prey was not recorded by items
+	# if this is the case for all studies, the dataframe will
+	# be empty (we filtered it for Diet_Type == Items) and 
+	# since we have no data we ignore this
+    if entries.empty:
+        return None
+
     total_items = entries["Item_Sample_Size"].sum()
     food_frequency = entries.groupby(group_by).sum()["Item_Sample_Size"]
-    print(food_frequency)
-    return (food_frequency.max(), food_frequency.idxmax())
 
-print(find_commonest_food("Bald Eagle"))
+	# the empty string in the dataset represents
+	# that the species of the prey item is unknown
+	# if this is the case, we skip bird species
+    if food_frequency.idxmax() == "" or total_items == 0:
+        return None
 
-# print(data[data["Diet_Type"] == "Items"].iloc[1])
-print(data[data["Common_Name"] == "Bald Eagle"].groupby("Diet_Type").sum())
+    return (food_frequency.idxmax(), food_frequency.max() / total_items)
+
+
+if __name__ == "__main__":
+    print(Card("hallo?", ["ja", "nee", "hallo", "doei"], 0))
+    print("parsing data...")
+    data = pyreadr.read_r("dietdb.rda")["dietdb"]
+    species = data["Common_Name"].drop_duplicates()
+    for s in species:
+        if (x := find_commonest_food(s)) is not None:	
+            commonest_food, normalized_frequency = x 
+            print(f"De meest voorkomende voedselklasse van {s} is {commonest_food}, met {normalized_frequency} van de prooiobjecten")
+
+
