2c447352d9aebae80d091f6bcb8dc20277b46428
Tars <tars.nijman@gmail.com>
2025-02-26 09:40:35 +0100

    coupled card gen with bird facts; TODO: fix answer randomization

--- bird_facts.py
+++ bird_facts.py
@@ -4,13 +4,16 @@ import pandas as pd
 import pyreadr
 
 class Card:
-    def __init__(self, question: str, answers: list[str], correct_answer_index: int):
+    def __init__(self, question: str, incorrect_answers: pd.Series, correct_answer: str, shown_answers: int = 4):
         self.question = question
-        self.answers = answers
-        self.correct_answer_index = correct_answer_index 
+        self.incorrect_answers = incorrect_answers
+        self.correct_answer = correct_answer 
+        self.shown_answers = shown_answers
 
     def __str__(self):
-        return self.question + "\n" + "\n".join([f"{l}: {a}" for l, a in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", random.sample(self.answers, len(self.answers)))])
+        return self.question 
+            + "\n" 
+            + "\n".join([f"{l}: {a}" for l, a in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", random.sample(self.incorrect_answers + [self.correct_answer], self.shown_answers))])
         
 
 def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, np.float64):
@@ -37,13 +40,17 @@ def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, np.
 
 
 if __name__ == "__main__":
-    print(Card("hallo?", ["ja", "nee", "hallo", "doei"], 0))
     print("parsing data...")
     data = pyreadr.read_r("dietdb.rda")["dietdb"]
+
     species = data["Common_Name"].drop_duplicates()
+    prey_classes = data["Prey_Class"].drop_duplicates()
+
     for s in species:
         if (x := find_commonest_food(s)) is not None:	
             commonest_food, normalized_frequency = x 
-            print(f"De meest voorkomende voedselklasse van {s} is {commonest_food}, met {normalized_frequency} van de prooiobjecten")
+            incorrect_answers = prey_classes[prey_classes != commonest_food].to_list()
+            card = Card(f"What does the {s} eat?", incorrect_answers, commonest_food) 
+            print(card)
 
 
