dc7886ffec6d5ab561977bc8556c1f3b7c1be44a
Tars <tars.nijman@gmail.com>
2025-03-05 09:41:48 +0100

    program can now export questions to svg

--- bird_facts.py
+++ bird_facts.py
@@ -12,11 +12,27 @@ class Card:
         self.total_answers_shown = total_answers_shown
 
     def __str__(self):
+        answers = self.get_answers()
+        return self.question + "\n" + "\n".join([f"{l}: {a}" 
+            for l, a in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", answers)]) + f"\n correct answer: {self.correct_answer}"
+
+    def get_answers(self, amount: int = 4):
         answers = random.sample(self.incorrect_answers, self.total_answers_shown - 1)
         answers.append(self.correct_answer)
         random.shuffle(answers)
-        return self.question + "\n" + "\n".join([f"{l}: {a}" 
-            for l, a in zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", answers)]) + f"\n correct answer: {self.correct_answer}"
+        return answers
+        
+        
+#def wrap_text(text: str, max_line_length: int) -> str:
+#    total_length = 0
+#    final_string = ""
+#    for word in text.split():
+#        total_length += len(word)
+#        if total_length > max_line_length:
+#            final_string += "\n" + word
+#            total_length = 0
+#        else:
+#            final_string += word
         
 
 def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, np.float64):
@@ -41,10 +57,24 @@ def find_commonest_food(species: str, *args, group_by="Prey_Class") -> (str, np.
 
     return (food_frequency.idxmax(), food_frequency.max() / total_items)
 
-def make_card(card: Card, template_path: str = "./trivia_card_template.svg"):
+
+def make_card(card: Card, template_path: str = "./trivia_card_template.svg") -> str:
     svg_tree = etree.parse(template_path)
-    print(svg_tree)
+    root = svg_tree.getroot()
+    q  = root[3]
+    a1 = root[4]
+    a2 = root[5]
+    a3 = root[6]
+    a4 = root[7]
+
+    answers = list(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", card.get_answers()))
+    correct_letter = next(x[0] for x in answers if x[1] == card.correct_answer)
     
+    q.text = card.question
+    [a1.text, a2.text, a3.text, a4.text] = list(map(lambda x: f"{x[0]}: {x[1]}", answers))
+    etree.ElementTree(root).write(card.question, pretty_print=True)
+
+    return correct_letter
 
 
 if __name__ == "__main__":
@@ -55,16 +85,12 @@ if __name__ == "__main__":
     prey_classes = data["Prey_Class"].drop_duplicates()
 
     # generate questions of type "What does [species] eat the most?"
-    for s in species:
+    for (i, s) in enumerate(species):
         if (x := find_commonest_food(s)) is not None:	
             commonest_food, normalized_frequency = x 
             incorrect_answers = prey_classes[prey_classes != commonest_food].to_list()
-            card = Card(f"What does the {s} eat the most?", incorrect_answers, commonest_food) 
-            print(card)
-            make_card(card)
-            
-      
-            
+            card = Card(f"{i}. What does the {s} eat the most?", incorrect_answers, commonest_food) 
+            print(make_card(card))
         break
 
 
